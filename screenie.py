a151df210acca1417cfaaa2971883d3c51f25687
Nikhil Joglekar <nikhiljoglekar@Utexas.edu>
2015-08-06 16:30:47 -0500

    image code scraping

---
+++ screenie.py
@@ -0,0 +1,27 @@
+﻿import urllib, urllib2, cookielib
+from BeautifulSoup import BeautifulSoup
+from PIL import Image
+
+#im = Image.open(image)
+#im.show()
+
+def getImgCodes(url):
+	cj = cookielib.CookieJar()
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+
+	html_dump = opener.open(url)
+
+	imgCodes = []
+	tree = BeautifulSoup(html_dump.read())
+	for link in tree.findAll('a'):
+		view = str(link.get('href'))
+		if 'Packages' in view and not 'GroupImage=True' in view:
+			imgCodes.append(view[14:39])
+
+	return imgCodes
+
+def cropImg(url):
+	return
+
+if __name__ == '__main__':
+	print imgCodes
